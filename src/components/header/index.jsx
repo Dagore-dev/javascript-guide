00f3d4defe13dc3a63200c0f0164015ebff4ec1c
Dagore <david.gomez.redondo.dev@gmail.com>
2021-12-01 12:20:50 +0100

    h2 con nav y formateo con prettier

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,22 +1,25 @@
 import { NavLink } from "solid-app-router";
 
-export default function Header () {
-    return (
-        <header>
-        <h1>Guía de Javascript para DAW1</h1>
-        <nav>
-          <ul>
-            <li>
-              <NavLink href="/">Principal</NavLink>
-            </li>
-            <li>
-              <NavLink href="/lo-mas-basico">Lo más básico</NavLink>
-            </li>
-            <li>
-              <NavLink href="/uso-de-objetos-integrados">Uso de objetos integrados</NavLink>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    );
+export default function Header() {
+  return (
+    <header>
+      <h1>Guía de Javascript para DAW1</h1>
+      <nav>
+        <h2>Navegación</h2>
+        <ul>
+          <li>
+            <NavLink href="/">Principal</NavLink>
+          </li>
+          <li>
+            <NavLink href="/lo-mas-basico">Lo más básico</NavLink>
+          </li>
+          <li>
+            <NavLink href="/uso-de-objetos-integrados">
+              Uso de objetos integrados
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
